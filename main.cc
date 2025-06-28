05e024ecee9c755a19403f63a95a70a5a38eee31
Yogesh Vishwakarma <yogeshkvishwakarma@gmail.com>
2025-06-28 20:33:00 +0530

    Enhance VSCode configuration and update main program to read input for dynamic output

--- main.cc
+++ main.cc
@@ -3,6 +3,11 @@ using namespace std;
 typedef vector<int> vi;
 
 int main() {
-    cout << "Hello Duniya";
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << i + 1 << ". Hello World.\n";
+    }
+
     return 0;
 }
