0e11a7cf1ffbe66df2378fafbfca064055a60392
Yogesh Vishwakarma <yogeshkvishwakarma@gmail.com>
2025-09-15 22:02:25 +0530

    Refactor Sample class formatting for improved readability and consistency

--- cpp/object_oriented_pragramming.cpp
+++ cpp/object_oriented_pragramming.cpp
@@ -5,25 +5,25 @@ using namespace std;
 typedef long long int ll;
 typedef vector<int> vc;
 typedef pair<int, int> pr;
-class Sample
-{
-private:      //access modifire
-    int a, b; //data member
-public:       //assess Modifire
-    //Constructers
-    Sample() {}                                                  //Default or non perameterized constructer
-    Sample(int a, int b) { this->a = a, this->b = b; }           //parameterized constructer
-    Sample(Sample &temp) { this->a = temp.a, this->b = temp.b; } //copy constructer
-    //Property Functions
-    //accessor
+
+class Sample {
+   private:    // access modifire
+    int a, b;  // data member
+   public:     // assess Modifire
+    // Constructers
+    Sample() {}                                                   // Default or non perameterized constructer
+    Sample(int a, int b) { this->a = a, this->b = b; }            // parameterized constructer
+    Sample(Sample &temp) { this->a = temp.a, this->b = temp.b; }  // copy constructer
+    // Property Functions
+    // accessor
     int geta() { return a; }
     int getb() { return b; }
     // Mutator
     void seta(int a) { this->a = a; }
     void setb(int b) { this->b = b; }
 };
-int main()
-{
+
+int main() {
     freopen("#input.txt", "r++", stdin);
     freopen("#output.txt", "w++", stdout);
     ios_base::sync_with_stdio(false);
@@ -35,7 +35,7 @@ int main()
     cout << s1.geta() << " " << s1.getb() << endl;
     Sample *s2 = &s1;
     cout << s2->geta() << " " << s2->getb() << endl;
-    Sample *s3 = new Sample; //Sample()
+    Sample *s3 = new Sample;  // Sample()
     s3->seta(30);
     s3->setb(40);
     cout << s3->geta() << " " << s3->getb() << endl;
