3e3df83f059b55fed4920b26e5a7b9255797c95d
Yogesh Vishwakarma <yogeshkvishwakarma@gmail.com>
2025-11-04 23:09:25 +0530

    Implement function to find missing number in an array with example usage

--- concepts/Step_3_Solve_Problems_on_Arrays_Easy_-_Medium_-_Hard/SubStep_1_Easy/Problem-10_Find_missing_number_in_an_array.cc
+++ concepts/Step_3_Solve_Problems_on_Arrays_Easy_-_Medium_-_Hard/SubStep_1_Easy/Problem-10_Find_missing_number_in_an_array.cc
@@ -7,6 +7,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int findMissingNum(vector<int>& nums) {
+    int n = *max_element(nums.begin(), nums.end()), sum = accumulate(nums.begin(), nums.end(), 0), actualSum = (n * (n + 1)) / 2;
+    return actualSum - sum;
+}
+
 int main() {
+    vector<int> nums = {0, 2, 3, 1, 4};
+    cout << findMissingNum(nums) << " ";
     return 0;
 }
