f022034c1cfcfc8e9074525278dfabb7f12aa09e
Yogesh Vishwakarma <yogeshkvishwakarma@gmail.com>
2025-11-24 20:21:36 +0530

    Implement Minimum Platforms and Job Sequencing solutions with example usage

--- concepts/Step_12_Greedy_Algorithms_Easy,_Medium_Hard/SubStep_2_Medium_Hard/Problem-4_Minimum_number_of_platforms_required_for_a_railway.cc
+++ concepts/Step_12_Greedy_Algorithms_Easy,_Medium_Hard/SubStep_2_Medium_Hard/Problem-4_Minimum_number_of_platforms_required_for_a_railway.cc
@@ -7,6 +7,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int minPlatform(vector<int>& arr, vector<int>& dep) {
+    // int maxPlatforms = 0;
+    // for (int i = 0; i < arr.size(); i++) {
+    //     int count = 1;
+    //     for (int j = i + 1; j < dep.size(); j++) {
+    //         // bool a = arr[i] >= arr[j] && dep[j] >= dep[i],
+    //         //      b = arr[i] <= arr[j] && dep[j] >= dep[i];
+    //         //  c = arr[i] >= arr[j] && dep[j] <= dep[i],
+    //         //  d = arr[i] <= arr[j] && dep[j] <= dep[i];
+    //         bool a = arr[i] >= arr[j] && arr[i] <= dep[j],
+    //              b = arr[j] >= arr[i] && arr[j] <= dep[i];
+    //         if (a || b) {
+    //             count++;
+    //         }
+    //         maxPlatforms = max(maxPlatforms, count);
+    //     }
+    // }
+
+    int i = 0, j = 0, count = 0, maxPlatforms = 0;
+    sort(arr.begin(), arr.end());
+    sort(dep.begin(), dep.end());
+    while (i < arr.size()) {
+        if (arr[i] <= dep[j]) {
+            count++, i++;
+        } else {
+            count--, j++;
+        }
+        maxPlatforms = max(maxPlatforms, count);
+    }
+    return maxPlatforms;
+}
+
 int main() {
+    vector<int> arr = {900, 940, 950, 1100, 1500, 1800}, dep = {910, 1200, 1120, 1130, 1900, 2000};
+    cout << minPlatform(arr, dep) << endl;
     return 0;
 }
--- concepts/Step_12_Greedy_Algorithms_Easy,_Medium_Hard/SubStep_2_Medium_Hard/Problem-5_Job_sequencing_Problem.cc
+++ concepts/Step_12_Greedy_Algorithms_Easy,_Medium_Hard/SubStep_2_Medium_Hard/Problem-5_Job_sequencing_Problem.cc
@@ -7,6 +7,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> jobSequencing(vector<int>& deadline, vector<int>& profit) {
+    class data {
+       public:
+        int deadline;
+        int profit;
+        data(int d, int p) {
+            this->deadline = d;
+            this->profit = p;
+        }
+    };
+    int maxDeadline = 0;
+    vector<data*> combinedData;
+    for (int i = 0; i < profit.size(); i++) {
+        maxDeadline = max(deadline.at(i), maxDeadline);
+        combinedData.push_back(new data(deadline.at(i), profit.at(i)));
+    }
+    sort(combinedData.begin(), combinedData.end(), [](data* d1, data* d2) -> bool {
+        return d1->profit > d2->profit;
+    });
+    // int maxDeadline = *max_element(deadline.begin(), deadline.end());
+    vector<int> jobDeadSeq(maxDeadline + 1, -1);
+
+    int maxJob = 0, maxProfit = 0;
+    for (auto const& data : combinedData) {
+        int i = data->deadline;
+        while (i >= 1) {
+            if (jobDeadSeq[i] == -1) {
+                jobDeadSeq[i] = data->deadline;
+                maxProfit += data->profit;
+                maxJob += 1;
+                break;
+            }
+            i--;
+        }
+    }
+    return {maxJob, maxProfit};
+}
+
 int main() {
+    vector<int> arr = {4, 1, 1, 1}, dep = {20, 10, 40, 30}, res = jobSequencing(arr, dep);
+    cout << res[0] << " " << res[1];
     return 0;
 }
