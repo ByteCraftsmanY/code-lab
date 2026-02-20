55d7e61532a8a73ab564dfc31537d1881b0a2564
Yogesh Vishwakarma <yogeshkvishwakarma@gmail.com>
2026-02-21 00:05:00 +0530

    added hard problem

--- DSA/Strivers_A2Z_Sheet/Step_15_Graphs_Concepts_&_Problems/SubStep_2_Problems_on_BFS_DFS/Problem-10_Word_ladder_-_1.cc
+++ DSA/Strivers_A2Z_Sheet/Step_15_Graphs_Concepts_&_Problems/SubStep_2_Problems_on_BFS_DFS/Problem-10_Word_ladder_-_1.cc
@@ -7,6 +7,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// bfs way
+int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    unordered_set<string> st(wordList.begin(), wordList.end());
+    queue<pair<string, int>> q;
+    q.push(make_pair(beginWord, 1));
+    st.erase(beginWord);
+    while (!q.empty()) {
+        auto data = q.front();
+        q.pop();
+        string s = data.first;
+        int steps = data.second;
+        if (s == endWord) return steps;
+        for (int i = 0; i < s.size(); i++) {
+            char orignalChar = s[i];
+            for (int c = 'a'; c <= 'z'; c++) {
+                s[i] = c;
+                if (st.find(s) != st.end()) {
+                    q.push({s, steps + 1});
+                    st.erase(s);
+                }
+            }
+            s[i] = orignalChar;
+        }
+    }
+    return 0;
+}
+
+int helper(string beginWord, string endWord, unordered_set<string>& wordList, int steps) {
+    // cout << "called for " << beginWord << " with step " << steps << endl;
+    if (beginWord == endWord) {
+        return steps;
+    }
+    int r = INT_MAX;
+    for (int i = 0; i < beginWord.size(); i++) {
+        char oc = beginWord[i];
+        for (char c = 'a'; c <= 'z'; c++) {
+            beginWord[i] = c;
+            if (wordList.find(beginWord) != wordList.end()) {
+                wordList.erase(beginWord);
+                r = min(r, helper(beginWord, endWord, wordList, steps + 1));
+                // wordList.insert(beginWord);
+            }
+        }
+        beginWord[i] = oc;
+    }
+    return r;
+}
+
+// dfs way ladderLength2
+int ladderLength2(string beginWord, string endWord, vector<string>& wordList) {
+    unordered_set<string> st(wordList.begin(), wordList.end());
+    int r = helper(beginWord, endWord, st, 0);
+    if (r == INT_MAX) return 0;
+    return r;
+}
+
 int main() {
+    string beginWord = "hit", endWord = "cog";
+    vector<string> wordList = {"hot", "dot", "dog", "lot", "log", "cog"};
+    cout << ladderLength2(beginWord, endWord, wordList);
     return 0;
 }
--- DSA/Strivers_SDE_Sheet/Step_25_Dynamic_Programming/Problem_6_Maximum_sum_increasing_subsequence.cpp
+++ DSA/Strivers_SDE_Sheet/Step_25_Dynamic_Programming/Problem_6_Maximum_sum_increasing_subsequence.cpp
@@ -8,7 +8,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int helper(int prev, int curr, vector<int>& nums, vector<vector<int>>& dp) {
+    int n = nums.size();
+    if (curr >= n) return 0;
+
+    // return cache result
+    if (dp[prev + 1][curr] != -1) return dp[prev + 1][curr];
+
+    // non pick
+    int maxLen = helper(prev, curr + 1, nums, dp);
+
+    // pick
+    if (prev == -1 || nums[prev] < nums[curr]) {
+        maxLen = max(maxLen, helper(curr, curr + 1, nums, dp) + 1);
+    }
+    dp[prev + 1][curr] = maxLen;
+
+    return maxLen;
+}
+
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(n + 1, vector<int>(n, -1));
+    return helper(-1, 0, nums, dp);
+}
+
+// space optimized
+int lengthOfLIS2(vector<int>& nums) {
+    int n = nums.size(), maxLen = 0;
+    if (n == 0) return 0;
+    vector<int> dp(n, 1);
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        maxLen = max(dp[i], maxLen);
+    }
+    return maxLen;
+}
+
+// print sequence
+int lengthOfLIS3(vector<int>& nums) {
+    int n = nums.size(), maxLen = 0;
+    if (n == 0) return 0;
+    vector<int> dp(n, 1);
+
+    int idx = -1;
+    vector<int> path(n, 0);
+    for (int i = 0; i < n; i++) path[i] = i;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
+                dp[i] = dp[j] + 1;
+                path[i] = j;
+            }
+        }
+        if (dp[i] > maxLen) {
+            maxLen = dp[i];
+            idx = i;
+        }
+    }
+
+    auto printer = [](vector<int>& data) {
+        for (auto d : data) {
+            cout << d << " ";
+        }
+        cout << endl;
+    };
+    printer(nums);
+    printer(dp);
+    printer(path);
+
+    int i = maxLen - 1;
+    vector<int> sequence(maxLen, 0);
+    while (i >= 0) {
+        sequence[i--] = nums[idx];
+        idx = path[idx];
+    }
+    printer(sequence);
+
+    return maxLen;
+}
+
 int main() {
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    // nums = {0, 1, 0, 3, 2, 3};
+    // nums = {7, 7, 7, 7, 7, 7, 7};
+    cout << lengthOfLIS3(nums) << endl;
     return 0;
 }
-                
